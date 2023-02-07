72596632f0d8f1741b9c9359e67c2cbda73fda6c
Dixant Sharma <dixant.sharma@blumeglobal.com>
2023-02-07 14:48:40 -0800

    Updates

---
+++ precache-manifest.d60aa5e30ab29b3a54ccf49af23b13be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa498bc7c2b5543c276a9d7cf496221",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "83f0ed7ae12bb41373c9",
+    "url": "/meme-generator/static/css/main.02bbcb75.chunk.css"
+  },
+  {
+    "revision": "dd1987a924826b6f11b1",
+    "url": "/meme-generator/static/js/2.a7764693.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/meme-generator/static/js/2.a7764693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f0ed7ae12bb41373c9",
+    "url": "/meme-generator/static/js/main.3cccd2ed.chunk.js"
+  },
+  {
+    "revision": "946f8b2b4ddd05339bb0",
+    "url": "/meme-generator/static/js/runtime-main.27c45a7a.js"
+  },
+  {
+    "revision": "2f0d6df3a388a07745b5f14c84ec1a59",
+    "url": "/meme-generator/static/media/meme-generator.2f0d6df3.png"
+  }
+]);
